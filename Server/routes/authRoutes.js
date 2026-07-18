8e99c873c44b29b92f2043bf82d4f56abdd5f93a
vinithatirukuchuru <vinithatirukuchuru@gmail.com>
2026-07-19 00:18:10 +0530

    Delete Server directory

--- Server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
-
-// Route mapping
-router.post('/register', registerUser);
-router.post('/login', loginUser); // <-- ADD THIS LINE
-
-module.exports = router;
